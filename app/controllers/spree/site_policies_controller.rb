fecf572bf6b921b3f52a6c126053a37a8a0110b7
yaseen iqbal <p136011@nu.edu.pk>
2019-04-02 19:18:33 +0500

    Add dynamic content

--- app/controllers/spree/site_policies_controller.rb
+++ app/controllers/spree/site_policies_controller.rb
@@ -1,7 +1,19 @@
 class Spree::SitePoliciesController < Spree::StoreController
   def terms
+
+  	terms_contents     = Spree::PageContent.from_gen_slug('terms')
+	@terms_main_content = terms_contents.from_gen_slug('terms-main_heading-desc').last
+	@terms_para1_content = terms_contents.from_gen_slug('terms-para1').last
+	@terms_para2_content = terms_contents.from_gen_slug('terms-para2').last
+
   end
 
   def policies
+  	privacy_contents     = Spree::PageContent.from_gen_slug('privacy')
+	@privacy_main_content = privacy_contents.from_gen_slug('privacy-main_heading-desc').last
+	@privacy_para1_content = privacy_contents.from_gen_slug('privacy-para1').last
+	@privacy_para2_content = privacy_contents.from_gen_slug('privacy-para2').last
   end
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@ Spree::PageContent.find_or_create_by! title: "Register Your Black-Line Account N
 
 Spree::PageContent.find_or_create_by! title: "Join our mailing list", description: "", slug: "register-label-mailig-list"
 Spree::PageContent.find_or_create_by! title: "To find out more about our Privacy Policy", description: "", slug: "register-label-privacy"
-Spree::PageContent.find_or_create_by! title: "By clicking  Create Your Account  you agreee to our", description: "", slug: "register-label-terms"
+Spree::PageContent.find_or_create_by! title: "By clicking  Create Your Account  you agree to our", description: "", slug: "register-label-terms"
 Spree::PageContent.find_or_create_by! title: " If you already have an account  please", description: "", slug: "register-label-login"
-Spree::PageContent.find_or_create_by! title: " Just Some of the benifits  For Registeration", description: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum h", slug: "register-label-benifits"
+Spree::PageContent.find_or_create_by! title: " Just Some of the benifits  For Registeration", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum h", slug: "register-label-benifits"
+
+
 
 Spree::PageContent.find_or_create_by! title: "here", description: "", slug: "register-btn-label-privacy"
 Spree::PageContent.find_or_create_by! title: "Create Your Account", description: "", slug: "register-btn-label-account"
@@ -12,8 +14,6 @@ Spree::PageContent.find_or_create_by! title: "Terms and Conditions", description
 Spree::PageContent.find_or_create_by! title: "Login", description: "", slug: "register-btn-label-login"
 Spree::PageContent.find_or_create_by! title: "Register with Facebook", description: "", slug: "register-btn-label-facebook"
 Spree::PageContent.find_or_create_by! title: "Register with Google", description: "", slug: "register-btn-label-google"
-
-
 Spree::PageContent.find_or_create_by! title: "Advertisement", description: "", slug: "register-label-advertisement"
 
 Spree::PageContent.find_or_create_by! title: "Make Your Digital Home Black Line Now", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer ", slug: "store-request-main-heading-desc"
@@ -24,3 +24,48 @@ Spree::PageContent.find_or_create_by! title: "Thanks, We'll Be In Touch Soon", d
 Spree::PageContent.find_or_create_by! title: "", description: ", thank you for registering your interest in being on our Marketplace.If you provided your email address we sent you an email confirming your interest.", slug: "store-request-message-paragraph-1"
 Spree::PageContent.find_or_create_by! title: "", description: "One of our amazing team will be in contact with you very soon to discuss the next steps.In the meantime we encourage you to look around and see why our stores love being on Black Line.", slug: "store-request-message-paragraph-2"
 Spree::PageContent.find_or_create_by! title: "Advertisement", description: "", slug: "store-request-message-label-advertisement"
+
+
+
+
+Spree::PageContent.find_or_create_by! title: "Login To Your Black-Line Account", description: "", slug: "login-main-heading-desc"
+Spree::PageContent.find_or_create_by! title: "I've Forgotten My Password", description: "", slug: "login-btn-forgot-password"
+
+Spree::PageContent.find_or_create_by! title: "Email Address", description: "", slug: "login-label-email-address"
+Spree::PageContent.find_or_create_by! title: "Password", description: "", slug: "login-label-password"
+
+
+Spree::PageContent.find_or_create_by! title: "Login", description: "", slug: "login-btn-login"
+Spree::PageContent.find_or_create_by! title: "Not Registered Yet? ", description: "", slug: "login-btn-not-registerd"
+Spree::PageContent.find_or_create_by! title: "No Problem", description: "", slug: "login-btn-no-problem"
+Spree::PageContent.find_or_create_by! title: " Click Here to Register", description: "", slug: "login-btn-register-link"
+Spree::PageContent.find_or_create_by! title: "Advertisement", description: "", slug: "login-label-advertisement"
+
+
+Spree::PageContent.find_or_create_by! title: "Forgotten Password", description: "Please enter your email address below and we will send you an email containing instructions on how to reset your password.", slug: "forgot-main-heading-desc"
+Spree::PageContent.find_or_create_by! title: "Email Address", description: "", slug: "forgot-label-email-address"
+Spree::PageContent.find_or_create_by! title: "Submit", description: "", slug: "forgot-label-btn-submit"
+Spree::PageContent.find_or_create_by! title: "Advertisement", description: "", slug: "forgot-label-advertisement"
+
+
+Spree::PageContent.find_or_create_by! title: "Lorem ipsum dolor sit amet", description: "", slug: "register-label-benifits-para-1"
+Spree::PageContent.find_or_create_by! title: "Excepteur sint occaecat cupidatat", description: "", slug: "register-label-benifits-para-2"
+Spree::PageContent.find_or_create_by! title: "non proident, sunt in culpa qui", description: "", slug: "register-label-benifits-para-3"
+Spree::PageContent.find_or_create_by! title: "At vero eos et accusamus et iusto", description: "", slug: "register-label-benifits-para-4"
+Spree::PageContent.find_or_create_by! title: "Nam libero tempore, cum soluta", description: "", slug: "register-label-benifits-para-5"
+
+
+
+
+Spree::PageContent.find_or_create_by! title: "Black Line Terms & Condition", description: "", slug: "terms-main_heading-desc"
+Spree::PageContent.find_or_create_by! title: "", description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin profess", slug: "terms-para1"
+Spree::PageContent.find_or_create_by! title: "", description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin profess", slug: "terms-para2"
+
+
+Spree::PageContent.find_or_create_by! title: "Black Line Privacy & Policy", description: "", slug: "privacy-main_heading-desc"
+Spree::PageContent.find_or_create_by! title: "", description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin profess", slug: "privacy-para1"
+Spree::PageContent.find_or_create_by! title: "", description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin profess", slug: "privacy-para2"
+
+
+
+Spree::PageContent.find_or_create_by! title: "Advertisement", description: "", slug: "register-select-label-advertisement"
